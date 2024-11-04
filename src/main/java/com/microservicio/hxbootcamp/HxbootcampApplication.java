40f6eba779b9bc84415c7e4ff2844facfeca232f
Pedro Osorio <pedro.osorio@pragma.com.co>
2024-11-04 18:35:24 -0500

    Creaçión inicial del proyecto

---
+++ src/main/java/com/microservicio/hxbootcamp/HxbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.hxbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HxbootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/hxbootcamp/HxbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.hxbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
